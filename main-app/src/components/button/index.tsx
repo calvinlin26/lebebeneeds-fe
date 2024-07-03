55602002099cc98674862b5d43eb7ab72c2dd615
mdudhin <muhammaddhiya.udhin@gmail.com>
2024-07-03 16:01:07 +0700

    fix: button issue

--- main-app/src/components/button/index.tsx
+++ main-app/src/components/button/index.tsx
@@ -12,16 +12,12 @@ export interface ButtonProps
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : "button";
-    return (
-      <Comp
-        className={cn(buttonVariants({ variant, size, className }))}
-        ref={ref}
-        {...props}
-      />
-    );
+    const combinedClassName = cn(buttonVariants({ variant, size }), className);
+
+    return <Comp className={combinedClassName} ref={ref} {...props} />;
   }
 );
 
 Button.displayName = "Button";
 
-export { Button };
+export { Button, buttonVariants };
