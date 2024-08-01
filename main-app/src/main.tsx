ecbd8bfc03aeedc4ecd7de9c81d1f6ee2c98568d
mdudhin <muhammaddhiya.udhin@gmail.com>
2024-08-01 16:23:38 +0700

    feature: add close button on the toaster

--- main-app/src/main.tsx
+++ main-app/src/main.tsx
@@ -12,6 +12,6 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <TokenProvider>
       <RouterProvider router={router} />
     </TokenProvider>
-    <Toaster richColors />
+    <Toaster richColors closeButton />
   </React.StrictMode>
 );
